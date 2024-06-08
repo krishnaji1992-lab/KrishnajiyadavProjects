5d31986a649456c8b9abd1e13c240eba308cdb80
krishna ji <krishnaji1992@gmail.com>
2024-06-08 14:44:45 +0530

    save all microservices project

---
+++ Section8/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,30 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Accounts extends BaseEntity {
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+
+
+
+}
---
+++ Section_10/accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,30 @@
+package com.eazybytes.accounts.mapper;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDtoDto) {
+        customerDetailsDtoDto.setName(customer.getName());
+        customerDetailsDtoDto.setEmail(customer.getEmail());
+        customerDetailsDtoDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDtoDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+
+}
